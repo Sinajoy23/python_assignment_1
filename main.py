3dae4172fa0ccfd9fe590532ec5f9c4458d5aa6e
Sinajoy23 <sinajoy.sj@gmail.com>
2025-06-07 00:03:42 -0400

    My first Python assignment

---
+++ main.py
@@ -0,0 +1,39 @@
+# Task 1 - Variables
+name = "Joy"
+age = 30
+height = 5.8
+print()
+print(f"Hey there, my name is {name}! I'm {age} years old and {height} feet tall.")
+
+
+#Task 2 - Operators 
+
+n1 = 10
+n2 = 3
+
+# Addition
+print("The sum of", n1, "and", n2, "is", n1 + n2)
+print()
+# Subtraction
+print("When you subtract", n2, "from", n1, "you get", n1 - n2)
+print()
+# Multiplication
+print("Multiplying", n1, "and", n2, "gives", n1 * n2)
+print()
+# Division
+print("Dividing", n1, "by", n2, "gives", n1 / n2)
+print()
+print()
+
+# Task 3 - Conditional Statements
+
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("This number is positive. Awesome!")
+elif number < 0:
+    print("This number is negative. Better luck next time!")
+else:
+    print("Zero it is. A perfect balance!")
+
